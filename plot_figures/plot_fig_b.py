11c1a70d51ea74c2c5716d1dcb5a2f6c1029fdbc
Andrew Gerard Twelves <51830799+atwelves@users.noreply.github.com>
2025-08-13 10:22:18 +0300

    Update plot_fig_b.py

--- plot_figures/plot_fig_b.py
+++ plot_figures/plot_fig_b.py
@@ -10,6 +10,7 @@ stations = ['Kemi','Oulu','Raahe',
             'Degerby',
             'Hanko','Helsinki','Porvoo','Hamina']
 
+# different marker for each station
 markers = ['v','^','>',
            '<','8',
            'p','s','h',
@@ -25,21 +26,30 @@ custom = [( 27/255,158/255,119/255),( 27/255,158/255,119/255),( 27/255,158/255,1
 
 plt.figure(figsize=(10,10))
 i = -1
+# declare array for mesoscale component of tide gauge transform
 mes_obs = np.zeros((13))
+# declare array for synoptic component of tide gauge transform
 syn_obs = np.zeros((13))
 for st in stations:
     i = i+1
+    # read in mesoscale transform of tide gauge data
     ds = xr.open_mfdataset('{}_obs_meso*'.format(st))
+    # convert to cm², sum over frequencies and average over year
     mes_obs[i] = 10000*ds.__xarray_dataarray_variable__.sum(dim='freq').mean(dim='date').values
+    # read in synoptic transform of tide gauge data
     ds = xr.open_mfdataset('{}_obs_syno*'.format(st))
+    # convert to cm², sum over frequencies and average over year
     syn_obs[i] = 10000*ds.__xarray_dataarray_variable__.sum(dim='freq').mean(dim='date').values
+    # plot synoptic against mesoscale
     plt.scatter(mes_obs[i],syn_obs[i],s=250,marker=markers[i],c=custom[i],edgecolors='k')
 
+# find linear relation between synoptic and mesoscale variability in Gulf of Bothnia
 gob_fit = np.polyfit(mes_obs[0:8],syn_obs[0:8],1)
 gob_x = np.linspace(0,1000,10)
 gob_y = gob_fit[1] + gob_fit[0]*gob_x
 plt.plot(gob_x,gob_y,c=custom[0],linestyle=':',label='GoB: syn={:.0f}+{:.1f}*meso'.format(gob_fit[1],gob_fit[0]),linewidth=5,alpha=0.5)
 
+# find linear relation between synoptic and mesoscale variability in Gulf of Finland
 gof_fit = np.polyfit(mes_obs[9:13],syn_obs[9:13],1)
 gof_x = np.linspace(0,1000,10)
 gof_y = gof_fit[1] + gof_fit[0]*gof_x
@@ -60,21 +70,31 @@ plt.savefig('ratios_obs.png')
 
 plt.figure(figsize=(10,10))
 i = -1
+
+# declare array for mesoscale component of model output
 mes_mod = np.zeros((13))
+# declare array for synoptic component of model output
 syn_mod = np.zeros((13))
 for st in stations:
     i = i+1
+    # read in mesoscale transform of model output
     ds = xr.open_mfdataset('{}_mod_meso*'.format(st))
+    # convert to cm², sum over frequencies and average over year
     mes_mod[i] = 10000*ds.__xarray_dataarray_variable__.sum(dim='freq').mean(dim='date').values
+    # read in synoptic transform of model output
     ds = xr.open_mfdataset('{}_mod_syno*'.format(st))
+    # convert to cm², sum over frequencies and average over year
     syn_mod[i] = 10000*ds.__xarray_dataarray_variable__.sum(dim='freq').mean(dim='date').values
+    # plot synoptic against mesoscale
     plt.scatter(mes_mod[i],syn_mod[i],s=250,marker=markers[i],c=custom[i],edgecolors='k')
 
+# find linear relation between synoptic and mesoscale variability in Gulf of Bothnia
 gob_fit = np.polyfit(mes_mod[0:8],syn_mod[0:8],1)
 gob_x = np.linspace(0,1000,10)
 gob_y = gob_fit[1] + gob_fit[0]*gob_x
 plt.plot(gob_x,gob_y,c=custom[0],linestyle=':',label='GoB: syn={:.0f}+{:.1f}*meso'.format(gob_fit[1],gob_fit[0]),linewidth=5,alpha=0.5)
 
+# find linear relation between synoptic and mesoscale variability in Gulf of Finland
 gof_fit = np.polyfit(mes_mod[9:13],syn_mod[9:13],1)
 gof_x = np.linspace(0,1000,10)
 gof_y = gof_fit[1] + gof_fit[0]*gof_x
@@ -91,17 +111,22 @@ plt.ylim(0,3000)
 plt.xlim(0,1000)
 plt.savefig('ratios_mod.png')
 
-# now plot anomaly (UNFILTERED)
+# now plot anomaly
 
 plt.figure(figsize=(10,10))
 i = -1
+# declare array for mesoscale component of model-data anomaly
 mes = np.zeros((13))
+# declare array for synoptic component of model-data anomaly
 syn = np.zeros((13))
 plt.fill_between([-200,-100,0],[-200,-100,0],[0,0,0],color=(0.9,0.9,0.9))#label='syn = meso')
 for st in stations:
     i = i+1
+    # find mesoscale component of anomaly
     mes = mes_mod[i] - mes_obs[i]
+    # find synoptic component of anomaly
     syn = syn_mod[i] - syn_obs[i]
+    # plot synoptic anomaly against mesoscale anomaly
     plt.scatter(mes,syn,s=250,marker=markers[i],c=custom[i],edgecolors='k')
 plt.ylabel('Synoptic scale variability anomaly (cm²)',fontsize=25)
 plt.xlabel('Mesoscale variability anomaly (cm²)',fontsize=25)
@@ -126,14 +151,18 @@ plt.xlabel('Mesoscale dispersion (m²)',fontsize=25)
 plt.grid()
 plt.savefig('ratios_anom_rel.png')
 
+### --- Map of tide gauge stations --- ###
+
 ds = xr.open_dataset('anomaly_maps/diff_mld_202009_mean.nc')
 mld = ds.mld.values
+# read in grid
 lat = ds.nav_lat_grid_T.values
 lon = ds.nav_lon_grid_T.values
 
 lat[lat < 0] = np.nan
 lon[lon < 0] = np.nan
 
+# convert 2d to 1d
 lat = np.nanmean(lat,1)
 lon = np.nanmean(lon,0)
 
@@ -154,6 +183,7 @@ for st in stations:
     ds = xr.open_dataset('data/BO_TS_TG_{}_201910.nc'.format(st))
     lat = ds.LATITUDE.values
     lon = ds.LONGITUDE.values
+    # position of each tide gauge station with label
     plt.scatter(lon,lat,s=250,marker=markers[i],label='{}'.format(st),c=custom[i],edgecolors='k')
 
 plt.legend(fontsize=15)
